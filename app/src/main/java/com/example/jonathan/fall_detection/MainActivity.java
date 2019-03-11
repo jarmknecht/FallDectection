38b11f4a2e6002217a2148b978648339c54cff3b
Jonathan Armknecht <jonathan.armknecht@gmail.com>
2019-03-11 13:29:01 -0600

    fixed app crash falling detection works now

--- app/src/main/java/com/example/jonathan/fall_detection/MainActivity.java
+++ app/src/main/java/com/example/jonathan/fall_detection/MainActivity.java
@@ -1,10 +1,10 @@
+//Referenced http://www.jcomputers.us/vol9/jcp0907-07.pdf for algorithm
 package com.example.jonathan.fall_detection;
 
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +14,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
-    private Sensor senseAccel;
+    private TextView text;
     static int sensorValuesSize = 70;
     float accelValuesX[] = new float[sensorValuesSize];
     float accelValuesY[] = new float[sensorValuesSize];
@@ -28,31 +28,46 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        text = (TextView) findViewById(R.id.Fall);
         //Get the systems sensor
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         //get accel sensor and register it an check it every 2 ms at least
         sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_UI);
-
+                SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-//TODO: App crashes here fix it maybe need to unregister listener?
     @Override
     public void onSensorChanged(SensorEvent event) {
+        Sensor _sensor = event.sensor;
         double rootSquare = 0.0;
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+        if (_sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             index++;
+            //call protects against buffer overflow
+            if (index >= sensorValuesSize - 1) {
+                index = 0; //wrap index back around
+                sensorManager.unregisterListener(this);
+                sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                        SensorManager.SENSOR_DELAY_NORMAL);
+            }
+            //get acceleration minus GX on the x-axis
             accelValuesX[index] = event.values[0];
+            //get acceleration minus GY on the y-axis
             accelValuesY[index] = event.values[1];
+            //get acceleration minus GZ on the z-axis
             accelValuesZ[index] = event.values[2];
 
             rootSquare = Math.sqrt(Math.pow(accelValuesX[index], 2) + Math.pow(accelValuesY[index], 2) +
-             Math.pow(accelValuesZ[index], 2));
+                    Math.pow(accelValuesZ[index], 2));
 
             if (rootSquare < 2.0) {
-                Toast.makeText(this, "Fall Detected", Toast.LENGTH_LONG).show();
+                //Toast.makeText(this, "Fall Detected", Toast.LENGTH_LONG).show();
                 fallDetected = true;
+            }
+            if (fallDetected) {
+                text.setText("FALL DETECTED!");
+            }
+            else {
+                text.setText("Fall not Detected");
             }
         }
     }
