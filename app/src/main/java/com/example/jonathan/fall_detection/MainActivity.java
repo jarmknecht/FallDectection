ea64619c98f80bb2a39fb56c6e3fe3de7f5d1db0
Jonathan Armknecht <jonathan.armknecht@gmail.com>
2019-03-18 18:05:02 -0600

    got location based and skeleton sms service working

--- app/src/main/java/com/example/jonathan/fall_detection/MainActivity.java
+++ app/src/main/java/com/example/jonathan/fall_detection/MainActivity.java
@@ -129,7 +129,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }*/
         }
     }
-//TODO: ADD LOCATION BASED INFO
+
     public void startVoiceRecognitionActivity() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
